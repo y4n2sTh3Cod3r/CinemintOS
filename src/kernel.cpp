abf7342258c834ff14a6372b8cbfe19648f49985
Ethan Hill <ethan.hill1999@outlook.com>
2025-04-16 20:35:26 -0500

    tweaks

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -6,6 +6,20 @@
 #include "include/screens.h"
 #include "include/io.h"
 
+#include "drivers/system/include.h"
+#include "drivers/graphic/include.h"
+#include "drivers/ps2/include.h"
+#include "drivers/keyboard/keyboard.h"
+#include "drivers/mouse/mouse.h"
+#include "drivers/ide/include.h"
+#include "drivers/ahci/include.h"
+#include "drivers/storage/include.h"
+#include "drivers/filesystems/include.h"
+#include "drivers/sound/include.h"
+#include "drivers/network/include.h"
+#include "drivers/usb/include.h"
+#include "drivers/simple_communication/include.h"
+
 extern "C" void kernel_main(multiboot_info *mbi)
 {
     cls();
